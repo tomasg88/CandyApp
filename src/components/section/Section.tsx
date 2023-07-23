36d953668aa8a9ab7ea87af3d387d3ad262fde66
Tomás González <tomasg88@gmail.com>
2023-07-23 18:06:01 -0300

    linted files

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -38,13 +38,13 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
   },
+  sectionDescription: {
+    fontSize: 18,
+    fontWeight: '400',
+    marginTop: 8,
+  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
   },
 });
--- src/screens/Analyze.tsx
+++ src/screens/Analyze.tsx
@@ -1,10 +1,14 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {FC} from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import {RootStackParamList} from '../App';
 
 export const Analyze: FC<
   NativeStackScreenProps<RootStackParamList, 'Analyze'>
 > = () => {
-  return <View>Analyze</View>;
+  return (
+    <View>
+      <Text>Analyze</Text>
+    </View>
+  );
 };
