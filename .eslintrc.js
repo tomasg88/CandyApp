7ea4fb66b5c0d4f59f8ea926b8fc368de5cdc586
Tomás González <tomasg88@gmail.com>
2023-07-23 17:54:11 -0300

    eslint sort-props rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
-  root: true,
+  env: {
+    'react-native/react-native': true,
+  },
   extends: '@react-native-community',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-native'],
+  root: true,
+  rules: {
+    'react/jsx-sort-props': 2,
+  },
 };
