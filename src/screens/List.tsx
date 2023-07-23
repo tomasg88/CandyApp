cdb413a506eb49c4931a2bee48f4aa087cf6591f
Tomás González <tomasg88@gmail.com>
2023-07-23 19:27:06 -0300

    comments

--- src/screens/List.tsx
+++ src/screens/List.tsx
@@ -6,6 +6,10 @@ import {RootStackParamList} from '../App';
 export const List: FC<
   NativeStackScreenProps<RootStackParamList, 'List'>
 > = () => {
+  // 1. Loading screen when loading
+  // 2. Empty list if !loading && !list
+  // 3. List if !loading && list
+
   return (
     <View>
       <Text>List</Text>
